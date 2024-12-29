0c43e41a5c4dc511d4638c38fda0f4963ba3bc02
Siri Karra <siriraj2004@gmail.com>
2024-12-29 21:25:47 +0530

    samplemaven

---
+++ src/main/java/selabpractice/mavennewpractice/App.java
@@ -0,0 +1,13 @@
+package selabpractice.mavennewpractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
